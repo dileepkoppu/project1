c11abf02728fd2ab7eed7160cf4261488b23b430
dileepkoppu <72597065+dileepkoppu@users.noreply.github.com>
2022-03-26 12:20:09 +0530

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
-STATIC_ROOT = "static_root"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
